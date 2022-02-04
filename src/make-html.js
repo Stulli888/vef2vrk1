79f37fbf6a1520631d0349b1b96e4e2aeeb7a8c5
Sturla <sfm1@hi.is>
2022-02-04 18:49:16 +0000

    Minor tweaks

--- src/make-html.js
+++ src/make-html.js
@@ -49,7 +49,7 @@ export function makeTemplate(title, body) {
       <meta charset='utf-8'>
       <meta name='viewport' content='width=device-width, initial-scale=1'>
       <title>${title ?? ''}</title>
-      <link rel='stylesheet' href='../styles.css' type='text/css'>
+      <link rel='stylesheet' href='styles.css' type='text/css'>
     </head>
     <body>
     <div class='wrapper'>
